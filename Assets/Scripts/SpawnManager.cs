872486fd1e129942d7590c39adfd3b7be2657fde
Shubham <vijit101@gmail.com>
2020-04-11 09:59:03 +0530

    -Day4-Collision Detection

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,17 +6,25 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefab;
     private int spawnXMax = 18;
+    private float startSpawnDelay = 1;
+    private float spawnIntervalTime = .8f;
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startSpawnDelay, spawnIntervalTime);
+    }
     // Start is called before the first frame update
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0,animalPrefab.Length);           
-            GameObject animal = animalPrefab[animalIndex];
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnXMax, spawnXMax), animal.transform.position.y, Random.Range(17, 19));
-            Instantiate(animal, spawnPos,animal.transform.rotation);
-        }
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+        GameObject animal = animalPrefab[animalIndex];
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnXMax, spawnXMax), animal.transform.position.y, Random.Range(17, 19));
+        Instantiate(animal, spawnPos, animal.transform.rotation);
     }
 }
